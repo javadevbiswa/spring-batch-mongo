7b12d7ea85695cedd22fd97230e273b0b81d3925
Biswa Bhusan Sahoo <biswa.sahoo06@gmail.com>
2022-01-27 14:48:49 +0530

    Fixed Error

--- sbcsv-mongo/src/main/java/sb/csv/mg/App.java
+++ sbcsv-mongo/src/main/java/sb/csv/mg/App.java
@@ -7,27 +7,22 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-	 public static void main(String[] args) throws Exception { 
-	     
-	      String[] springConfig  =  { "config/jobConfig.xml" };  
-	      
-	      // Creating the application context object        
-	      ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);  
-	      
-	      // Creating the job launcher 
-	      JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher"); 
-	   
-	      // Creating the job 
-	      Job job = (Job) context.getBean("jobA"); 
-	   
-	      // Executing the JOB 
-	      JobExecution execution = jobLauncher.run(job, new JobParameters());
-	      System.out.println("Exit Status : " + execution.getStatus()); 
-	   } 
+public class App {
+	public static void main(String[] args) throws Exception {
+
+		String[] springConfig = { "config/jobConfig.xml" };
+
+		// Creating the application context object
+		ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+
+		// Creating the job launcher
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+
+		// Creating the job
+		Job job = (Job) context.getBean("jobA");
+
+		// Executing the JOB
+		JobExecution execution = jobLauncher.run(job, new JobParameters());
+		System.out.println("Exit Status : " + execution.getStatus());
+	}
 }
--- sbcsv-mongo/src/main/java/sb/csv/mg/mapper/EmployeeMapper.java
+++ sbcsv-mongo/src/main/java/sb/csv/mg/mapper/EmployeeMapper.java
@@ -6,17 +6,22 @@ import org.springframework.validation.BindException;
 
 import sb.csv.mg.model.Employee;
 
-public class EmployeeMapper implements FieldSetMapper<Employee>{
+public class EmployeeMapper implements FieldSetMapper<Employee> {
+
+	private static Boolean isFirst = Boolean.TRUE;
 
 	@Override
 	public Employee mapFieldSet(FieldSet fieldSet) throws BindException {
-		Employee e=new Employee();
-		
+		Employee e = new Employee();
+		if (isFirst) {
+			isFirst = Boolean.FALSE;
+			return e;
+		}
+
 		e.setEid(fieldSet.readInt(0));
 		e.setEname(fieldSet.readString(1));
 		e.setEsal(fieldSet.readDouble(2));
 		return e;
 	}
 
-	
 }
